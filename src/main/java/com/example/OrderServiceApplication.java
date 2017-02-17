6cedf709e48809facac5637808f5d480d99a4387
s.hermans <s.hermans@maxxton.com>
2017-02-17 13:57:17 +0100

    add proxy maven

--- src/main/java/com/example/OrderServiceApplication.java
+++ src/main/java/com/example/OrderServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
